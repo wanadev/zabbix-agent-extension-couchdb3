a90b3cf8d62aa4ecd9f762c29a0428041087305e
Fabien LOISON <flo@flogisoft.com>
2020-07-20 11:28:16 +0200

    Handles complexe values (histogram)

--- zabbix_agent_extension_couchdb3/couchdb.py
+++ zabbix_agent_extension_couchdb3/couchdb.py
@@ -30,12 +30,32 @@ def get_stats(host="localhost", port=5984, user="admin", password=None, proto="h
 def flatten_stats(stats_json, prefix="couchdb3"):
     for key in stats_json:
         if "value" in stats_json[key]:
+            value = stats_json[key]["value"]
+            desc = stats_json[key]["desc"]
             if type(stats_json[key]["value"]) is dict:
-                continue  # FIXME skip complex values
-            yield (
-                    "%s.%s" % (prefix, key),
-                    stats_json[key]["value"],
-                    stats_json[key]["desc"])
+                if "min" in value:
+                    yield (
+                        "%s.%s.min" % (prefix, key),
+                        value["min"],
+                        desc)
+                if "max" in value:
+                    yield (
+                        "%s.%s.max" % (prefix, key),
+                        value["max"],
+                        desc)
+                if "median" in value:
+                    yield (
+                        "%s.%s.median" % (prefix, key),
+                        value["median"],
+                        desc)
+                if "percentile" in value:
+                    for perc, count in value["percentile"]:
+                        yield (
+                            "%s.%s.percentile[%i]" % (prefix, key, perc),
+                            count,
+                            desc)
+            else:
+                yield ("%s.%s" % (prefix, key), value, desc)
         else:
             for item in flatten_stats(stats_json[key], "%s.%s" % (prefix, key)):  # noqa: E501
                 yield item
